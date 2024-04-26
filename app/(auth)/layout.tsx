204077207f87eb89c372de14545f1fd92ab6ec79
Isaac Song <isaacsongs10@gmail.com>
2024-04-26 15:11:04 -0700

    configure layout file for auth folder

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,13 @@
 import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
+import '../globals.css'
+
+const inter = Inter({ subsets : ["latin"] })
 
 export const metadata = {
     title: 'Threads',
     description: 'A Next.js 13 Meta Threads Application'
 }
-
-const inter = Inter({ subsets : ["latin"] })
 
 export default function RootLayout({ 
     children 
