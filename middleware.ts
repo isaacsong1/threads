2a6bcf1673e2010e503fdc6a2a7df9939547111d
Isaac Song <isaacsongs10@gmail.com>
2024-04-29 15:18:27 -0700

    change spacing

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 export default clerkMiddleware()
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+    // The following matcher runs middleware on all routes
+    // except static assets.
+    matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
