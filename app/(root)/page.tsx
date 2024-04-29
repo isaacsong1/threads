f5b5ff2e1aea5219fe2104fa97a4bdde4b164b8c
Isaac Song <isaacsongs10@gmail.com>
2024-04-29 15:50:29 -0700

    update root layout with clerkprovider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <ClerkProvider>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </ClerkProvider>
   )
 }
