3b5ba5c5a1dbce82be79fd6ae8221a822051fab6
Isaac Song <isaacsongs10@gmail.com>
2024-04-29 15:30:21 -0700

    integrate clerkMiddleware boilerplate

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-    publicRoutes: ['/', '/api/webhook/clerk'],
-    ignoredRoutes: ['/api/webhook/clerk']
-});
+export default clerkMiddleware();
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
