33b681757a6bc10578fb00332cf3c6a1962302e8
Isaac Song <isaacsongs10@gmail.com>
2024-04-30 14:38:40 -0700

    add image component from next image

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
 
 function Topbar() {
     return (
         <nav className="topbar">
             <Link href="/" className="flex items-center gap-4"></Link>
+            <Image src="/logo.svg" alt="logo" width={28} height={28} />
         </nav>
     )
 }
