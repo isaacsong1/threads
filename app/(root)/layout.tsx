258669b7a14d77a190de9ef473b31e602452cfab
Isaac Song <isaacsongs10@gmail.com>
2024-04-30 14:32:37 -0700

    import shared components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Bottombar from '@/components/shared/Bottombar'
 
 const inter = Inter({ subsets: ["latin"] });
 
