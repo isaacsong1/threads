fd22a90bb5fb753e31819e9cc37befc2742eb563
Isaac Song <isaacsongs10@gmail.com>
2024-05-01 01:20:28 -0700

    append redirectUrl property to SignOutButton

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -36,7 +36,8 @@ function LeftSidebar() {
             </div>
             <div className="mt-10 px-6">
                 <SignedIn>
-                    <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+                {/* signOutCallback={() => router.push('/sign-in')} */}
+                    <SignOutButton redirectUrl='sign-in'>
                         <div className="flex cursor-pointer gap-4 p-4">
                             <Image 
                                 src="assets/logout.svg"
