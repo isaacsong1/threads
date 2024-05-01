123032b4d397525268c0552c88a657824491b684
Isaac Song <isaacsongs10@gmail.com>
2024-05-01 01:31:19 -0700

    add beginner code for right side bar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,13 @@
 function RightSidebar() {
-    return <h1>RightSidebar</h1>
+    return (
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start">
+                <h3>Suggested Communities</h3>
+
+            </div>
+
+        </section>
+    )
 }
 
 export default RightSidebar;
