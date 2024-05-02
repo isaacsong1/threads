7b8caa3f0cbd183108704751e7fe06a5547949c7
Isaac Song <isaacsongs10@gmail.com>
2024-05-02 14:47:12 -0700

    create user validation ts file

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import { 
     Form
 } from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 // Defines Props as an interface
 interface Props {
@@ -19,7 +20,9 @@ interface Props {
 
 // AccountProfile accepts user and btnTitle as type Props
 const AccountProfile = ({ user, btnTitle }: Props) => {
-    const form = useForm()
+    const form = useForm({
+        resolver:
+    })
 
     return (
         <Form>
