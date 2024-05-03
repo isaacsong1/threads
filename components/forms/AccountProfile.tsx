e806cc2f28b0051c63ca90e6526096b77dd79167
Isaac Song <isaacsongs10@gmail.com>
2024-05-03 15:36:57 -0700

    import form components

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,9 +1,17 @@
 "use client"
 
 import { useForm } from 'react-hook-form';
-import { 
-    Form
-} from '@/components/ui/form';
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UserValidation } from '@/lib/validations/user';
 
