5e4bdf127115e5d008dba1aa7c6be96a8d52c6d4
Isaac Song <isaacsongs10@gmail.com>
2024-05-06 15:15:23 -0700

    add form submit handler func

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -39,7 +39,12 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         }
     })
 
-    
+    // Submit handler
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
 
     return (
 
