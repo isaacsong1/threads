c47aed824648b43f5790300c2c7b3c3d54392691
Isaac Song <isaacsongs10@gmail.com>
2024-05-07 14:18:00 -0700

    add strictQuery attribute

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,5 +3,7 @@ import mongoose from 'mongoose';
 let isConnected = false; // variable to check if mongoose is connected
 
 export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
     
 }
