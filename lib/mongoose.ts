d7a0e05eb42fcd21079fbd739de9e968521aee15
Isaac Song <isaacsongs10@gmail.com>
2024-05-07 14:19:03 -0700

    add conditionals to console log connection status

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -5,5 +5,6 @@ let isConnected = false; // variable to check if mongoose is connected
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
 
-    
+    if (!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
+    if (isConnected) return console.log('Already connected to MongoDB');
 }
