d37c614a5628cc448c62d01113f17afd39be612e
Isaac Song <isaacsongs10@gmail.com>
2024-05-07 14:40:06 -0700

    add await User function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,8 +3,25 @@
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
-export async function updateUser(): Promise<void> {
+export async function updateUser(
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string,
+): Promise<void> {
     connectToDB();
 
-    await User.findOneAndUpdate()
+    await User.findOneAndUpdate(
+        { id: userId },
+        { 
+            username: username.toLowerCase(),
+            name,
+            bio,
+            image,
+            onboarded: true,
+        },
+        { upsert: true }
+    )
 }
