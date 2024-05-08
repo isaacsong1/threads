cebfca7efedbc576b8d60a2ef2494cd52268de1b
Isaac Song <isaacsongs10@gmail.com>
2024-05-08 15:35:48 -0700

    import fetchUser

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,3 +1,4 @@
+import { fetchUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
@@ -7,7 +8,7 @@ async function Page() {
     if (!user) return null
 
     const userInfo = await fetchUser(user.id);
-    
+
     return <h1 className="head-text">Create Thread</h1>
 }
 
