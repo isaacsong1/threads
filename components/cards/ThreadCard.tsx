2f533ee5fa55ba3b639ca43cd06f9ff001bbeda0
Isaac Song <isaacsongs10@gmail.com>
2024-05-15 14:33:15 -0700

    check if isComment and show length

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -33,7 +33,8 @@ const ThreadCard = ({
     author,
     community,
     createdAt,
-    comments
+    comments,
+    isComment,
 }: Props) => {
     return (
         <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
@@ -91,6 +92,14 @@ const ThreadCard = ({
                                     className="cursor-pointer object-contain"
                                 />
                             </div>
+
+                            {isComment && comments.length > 0 && (
+                                <Link href={`/thread/${id}`}>
+                                    <p className="mt-1 text-subtle-medium text-gray-1">
+                                        {comments.length} replies
+                                    </p>
+                                </Link>
+                            )}
                         </div>
                     </div>
                 </div>
