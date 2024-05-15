8b7f013f7881596ba38acd67faa9fe92399443e5
Isaac Song <isaacsongs10@gmail.com>
2024-05-15 14:27:25 -0700

    add threadcard structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchThreads } from "@/lib/actions/thread.actions";
 import { currentUser } from '@clerk/nextjs/server';
 
@@ -17,19 +18,21 @@ export default async function Home() {
         {result.threads.length === 0 ? (
           <p className="no-result">No threads found</p>
         ) : (
-          {result.threads.map((thread) => (
-            <ThreadCard 
-              key={thread._id}
-              id={thread._id}
-              currentUserId={user?.id}
-              parentId={thread.parentId}
-              content={thread.text}
-              author={thread.author}
-              community={thread.community}
-              createdAt={thread.createdAt}
-              comments={thread.children}
-            />
-          ))}
+          <>
+            {result.threads.map((thread) => (
+              <ThreadCard 
+                key={thread._id}
+                id={thread._id}
+                currentUserId={user?.id || ""}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+              />
+            ))}
+          </>
         )}
       </section>
     </>
