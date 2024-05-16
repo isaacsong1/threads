14144377c68b5e89e11f24c704143b8fe5824b5b
Isaac Song <isaacsongs10@gmail.com>
2024-05-16 14:39:44 -0700

    import props to Comment form

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,4 +1,10 @@
-const Comment = () => {
+interface Props {
+    threadId: string;
+    currentUserImg: string;
+    currentUserId: string;
+}
+
+const Comment = ({threadId, currentUserImg, currentUserId}: Props) => {
     return (
         <div>
             <h1 className="text-white">Comment Form</h1>
