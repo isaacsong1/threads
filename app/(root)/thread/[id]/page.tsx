cada180070d556e8e588136fb09d47bdd3404164
Isaac Song <isaacsongs10@gmail.com>
2024-05-16 14:37:16 -0700

    import comment component

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ThreadCard from "@/components/cards/ThreadCard";
+import Comment from "@/components/forms/Comment";
 import { fetchThreadById } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from '@clerk/nextjs/server'
