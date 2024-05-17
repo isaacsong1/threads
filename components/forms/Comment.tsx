b658f4ab9c461a8fd3e0e425cb11b4342f6ddd52
Isaac Song <isaacsongs10@gmail.com>
2024-05-17 13:28:59 -0700

    add boilerplate form for comment

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,3 +1,23 @@
+"use client"
+
+import { z } from "zod"
+import { useForm } from 'react-hook-form';
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { usePathname, useRouter } from 'next/navigation';
+// import { updateUser } from '@/lib/actions/user.actions'; 
+import { CommentValidation } from '@/lib/validations/thread';
+// import { addComment } from "@/lib/actions/thread.actions";
+
 interface Props {
     threadId: string;
     currentUserImg: string;
@@ -5,10 +25,59 @@ interface Props {
 }
 
 const Comment = ({threadId, currentUserImg, currentUserId}: Props) => {
+    const router = useRouter();
+    const pathname = usePathname();
+    
+    const form = useForm({
+        resolver: zodResolver(ThreadValidation),
+        defaultValues: {
+            thread: '',
+            accountId: userId,
+        }
+    })
+
+    const onSubmit =  async (values: z.infer<typeof ThreadValidation>) => {
+        await createThread({ 
+            text: values.thread,
+            author: userId,
+            communityId: null,
+            path: pathname
+        });
+
+        router.push("/");
+    }
+
     return (
-        <div>
-            <h1 className="text-white">Comment Form</h1>
-        </div>
+        <Form {...form}>
+            <form 
+                onSubmit={form.handleSubmit(onSubmit)} 
+                className="mt-10 flex flex-col justify-start gap-10"
+            >
+                {/* Form field for name */}
+                <FormField
+                    control={form.control}
+                    name="thread"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col w-full gap-3">
+                            <FormLabel className="text-base-semibold text-light-2">
+                                Content
+                            </FormLabel>
+                            <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                                <Textarea 
+                                    rows={15}
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <Button type="submit" className="bg-primary-500">
+                    Post Thread
+                </Button>
+            </form>
+        </Form>
     )
 }
 
