f96355774f3040c1b3724adb3e1b79725a91f861
Isaac Song <isaacsongs10@gmail.com>
2024-05-17 13:41:54 -0700

    fetch original thread and create new thread

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -101,3 +101,32 @@ export async function fetchThreadById(id: string) {
         throw new Error(`Error fetching thread: ${error.message}`)
     }
 }
+
+export async function addCommentToThread(
+    threadId:string,
+    commentText: string,
+    userId: string,
+    path: string,
+) {
+    connectToDB();
+
+    try {
+        // Find the original thread by its ID
+        const originalThread = await Thread.findById(threadId);
+
+        if (!originalThread) {
+            throw new Error("Thread not found");
+        }
+
+        // Create a new thread with the comment text
+        const commentThread = new Thread({
+            text: commentText,
+            author: userId,
+            parentId: threadId
+        })
+
+
+    } catch (error: any) {
+        throw new Error(`Error adding comment to thread: ${error.message}`)
+    }
+}
