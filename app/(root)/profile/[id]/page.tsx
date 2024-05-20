85ce58f58b6baba17966f13abdbc0fec22eaaa02
Isaac Song <isaacsongs10@gmail.com>
2024-05-20 16:17:59 -0700

    add tabs to profile

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -2,6 +2,9 @@ import ProfileHeader from '@/components/shared/ProfileHeader';
 import { fetchUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import Image from "next/image";
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { profileTabs } from '@/constants';
 
 async function Page({ params }: { params: {id: string}}) {
     const user = await currentUser();
@@ -20,6 +23,45 @@ async function Page({ params }: { params: {id: string}}) {
                 imgUrl={userInfo.image}
                 bio={userInfo.bio}
             />
+
+            <div className='mt-9'>
+                <Tabs defaultValue='threads' className='w-full'>
+                    <TabsList className='tab'>
+                        {profileTabs.map((tab) => (
+                            <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+                                <Image
+                                    src={tab.icon}
+                                    alt={tab.label}
+                                    width={24}
+                                    height={24}
+                                    className='object-contain'
+                                />
+                                <p className='max-sm:hidden'>{tab.label}</p>
+
+                                {tab.label === "Threads" && (
+                                    <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
+                                        {userInfo.threads.length}
+                                    </p>
+                                )}
+                            </TabsTrigger>
+                        ))}
+                    </TabsList>
+                    {/* {profileTabs.map((tab) => (
+                        <TabsContent
+                            key={`content-${tab.label}`}
+                            value={tab.value}
+                            className='w-full text-light-1'
+                        >
+                            @ts-ignore
+                            <ThreadsTab
+                                currentUserId={user.id}
+                                accountId={userInfo.id}
+                                accountType='User'
+                            />
+                        </TabsContent>
+                    ))} */}
+                </Tabs>
+            </div>
         </section>
     )
 }
--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -31,7 +31,14 @@ const ProfileHeader = ({ accountId, authUserId, name, username, imgUrl, bio,
                         <p className='text-base-medium text-gray-1'>@{username}</p>
                     </div>
                 </div>
+
+                {/* TODO: Community */}
+
             </div>
+            
+            <p className='mt-6 max-w-lg text-base-regular text-light-2'>{bio}</p>
+
+            <div className='mt-12 h-0.5 w-full bg-dark-3' />
         </div>
     )
 }
