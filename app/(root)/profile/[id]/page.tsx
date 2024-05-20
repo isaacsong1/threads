dab2035c964fddb49bc90c026ee4cf60c9053f7f
Isaac Song <isaacsongs10@gmail.com>
2024-05-20 15:50:20 -0700

    fetch user and check onboarding status

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,16 @@
-const Page = async () => {
+import { fetchUser } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+async function Page() {
+    const user = await currentUser();
+
+    if (!user) return null
+
+    const userInfo = await fetchUser(user.id);
+
+    if (!userInfo?.onboarded) redirect('/onboarding');
+
     return (
         <section>
             Profile
