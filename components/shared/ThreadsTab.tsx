3c8c4981d6f4a34e2085ecdd46288ddfb1f613f8
Isaac Song <isaacsongs10@gmail.com>
2024-05-20 16:49:39 -0700

    check author in ThreadCard

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -15,14 +15,22 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
 
     return (
         <section className="mt-9 flex flex-col gap-10">
-            {result.threads.map((thread) => (
+            {result.threads.map((thread: any) => (
                 <ThreadCard
                     key={thread._id}
                     id={thread._id}
                     currentUserId={currentUserId}
                     parentId={thread.parentId}
                     content={thread.text}
-                    author={thread.author} // todo
+                    author={
+                        accountType === "User"
+                            ? { name: result.name, image: result.image, id: result.id }
+                            : {
+                                name: thread.author.name,
+                                image: thread.author.image,
+                                id: thread.author.id,
+                            }
+                    }
                     community={thread.community} // todo
                     createdAt={thread.createdAt}
                     comments={thread.children}
