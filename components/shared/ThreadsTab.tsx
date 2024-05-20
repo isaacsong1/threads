632595b0b42d8424fbb69d826f5c3112bfc34a85
Isaac Song <isaacsongs10@gmail.com>
2024-05-20 16:37:37 -0700

    add Props to threadsTab

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,4 +1,12 @@
-const ThreadsTab = async () => {
+interface Props {
+    currentUserId: string;
+    accountId: string;
+    accountType: string;
+}
+
+const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+    // TODO: Fetch Profile Threads
+
     return (
         <section>
             ThreadsTab
