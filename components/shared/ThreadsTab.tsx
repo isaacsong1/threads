73937a4b114aa6e46ae1b2a64af2ecc91ac898d5
Isaac Song <isaacsongs10@gmail.com>
2024-05-20 16:44:32 -0700

    add ThreadCard

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,3 +1,7 @@
+import { fetchUserThreads } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import ThreadCard from "../cards/ThreadCard";
+
 interface Props {
     currentUserId: string;
     accountId: string;
@@ -5,11 +9,25 @@ interface Props {
 }
 
 const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
-    // TODO: Fetch Profile Threads
+    let result = await fetchUserThreads(accountId);
+
+    if (!result) redirect('/');
 
     return (
-        <section>
-            ThreadsTab
+        <section className="mt-9 flex flex-col gap-10">
+            {result.threads.map((thread) => (
+                <ThreadCard
+                    key={thread._id}
+                    id={thread._id}
+                    currentUserId={currentUserId}
+                    parentId={thread.parentId}
+                    content={thread.text}
+                    author={thread.author} // todo
+                    community={thread.community} // todo
+                    createdAt={thread.createdAt}
+                    comments={thread.children}
+                />
+            ))}
         </section>
     )
 }
