3d4316c36065d65cb6db75d5101221cb3df18d2e
Isaac Song <isaacsongs10@gmail.com>
2024-05-22 15:23:34 -0700

    add params to fetchUsers

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { SortOrder } from "mongoose";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
@@ -83,6 +84,30 @@ export async function fetchUserThreads(userId: string) {
                 ],
             });
         return threads;
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user threads: ${error.message}`);
+    }
+}
+
+export async function fetchUsers({ 
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc"
+}: {
+    userId: string;
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: SortOrder;
+}) {
+    try {
+        connectToDB();
+
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, "i");
     } catch (error: any) {
         throw new Error(`Failed to fetch user threads: ${error.message}`);
     }
