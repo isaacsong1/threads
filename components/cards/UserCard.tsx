5d6f567e4522c285e5c184f0039e37036ef36c02
Isaac Song <isaacsongs10@gmail.com>
2024-05-23 12:52:56 -0700

    navigate user to clicked profile

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import Image from "next/image";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
 interface Props {
     key: string;
@@ -10,6 +14,8 @@ interface Props {
 }
 
 const UserCard = ({ key, id, name, username, imgUrl, personType }: Props) => {
+    const router = useRouter();
+
     return (
         <article className="user-card">
             <div className="user-card_avatar">
@@ -21,7 +27,15 @@ const UserCard = ({ key, id, name, username, imgUrl, personType }: Props) => {
                     className="rounded-full"
                 />
 
+                <div className="flex-1 text-ellipsis">
+                    <h4 className="text-base-semibold text-light-1">{name}</h4>
+                    <p className="text-small-medium text-gray-1">@{username}</p>
+                </div>
             </div>
+
+            <Button className="user-card_btn" onClick={() => router.push(`/profile/${id}`)}>
+                View
+            </Button>
         </article>
     )
 }
