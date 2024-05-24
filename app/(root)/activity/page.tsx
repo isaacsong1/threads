b294daa202ee3ff706f990de433df12ac0c9a9da
Isaac Song <isaacsongs10@gmail.com>
2024-05-24 13:48:43 -0700

    check currentUser and onboarding status

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,17 @@
-const Page = async () => {
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import { profileTabs } from '@/constants';
+import ThreadsTab from '@/components/shared/ThreadsTab';
+import UserCard from '@/components/cards/UserCard';
+
+async function Page() {
+    const user = await currentUser();
+    if (!user) return null
+
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect('/onboarding');
+    
     return (
         <section>
             <h1 className="head-text mb-10">Activity</h1>
