4d33ca2ef6b5b4e804ca28ce9e983162a8985cec
Isaac Song <isaacsongs10@gmail.com>
2024-05-24 14:09:41 -0700

    map activity to Link elements

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,8 @@
-import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { fetchUser, getActivity } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs/server';
+import Link from 'next/link';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
-import { profileTabs } from '@/constants';
-import ThreadsTab from '@/components/shared/ThreadsTab';
-import UserCard from '@/components/cards/UserCard';
 
 async function Page() {
     const user = await currentUser();
@@ -13,10 +12,31 @@ async function Page() {
     if (!userInfo?.onboarded) redirect('/onboarding');
 
     // Get Activity
+    const activity = await getActivity(userInfo._id);
     
     return (
         <section>
             <h1 className="head-text mb-10">Activity</h1>
+
+            <section className="mt-10 flex flex-col gap-5">
+                {activity.length > 0 ? (
+                    <>
+                        {activity.map((activity) => (
+                            <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                                <article className="activity-card">
+                                    <Image 
+                                        src={activity.author.image}
+                                        alt="Profile Picture"
+                                        width={20}
+                                        height={20}
+                                        className="rounded-full object-cover"
+                                    />
+                                </article>
+                            </Link>
+                        ))}
+                    </>
+                ): <p>No activity yet</p>}
+            </section>
         </section>
     )
 }
