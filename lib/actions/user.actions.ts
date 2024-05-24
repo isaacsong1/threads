4461c217c71435b32b7629a1fa4553c08411ce12
Isaac Song <isaacsongs10@gmail.com>
2024-05-24 14:04:08 -0700

    implement getActivity logic

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -151,7 +151,22 @@ export async function getActivity(userId: string) {
         // Find all threads created by the user
         const userThreads = await Thread.find({ author: userId });
 
-        
+        // Collect all the child thread ids (replies) from the 'children'
+        const childThreadIds = userThreads.reduce((acc, userThread) => {
+            // Looping through userThread array and adding all the comments to our acc array
+            return acc.concat(userThread.children);
+        })
+
+        const replies = await Thread.find({
+            _id: { $in: childThreadIds },
+            author: { $ne: userId }
+        }).populate({
+            path: 'author',
+            model: User,
+            select: 'name image _id'
+        });
+
+        return replies;
     } catch (error: any) {
         throw new Error(`Failed to fetch user activity: ${error.message}`)
     }
