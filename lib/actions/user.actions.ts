1e10918c89e4a26fca68ef8d64573eefe175950c
Isaac Song <isaacsongs10@gmail.com>
2024-05-28 14:20:38 -0700

    update user and thread model for communities

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,31 +1,47 @@
-"use server"
+"use server";
 
 import { FilterQuery, SortOrder } from "mongoose";
+import { revalidatePath } from "next/cache";
+
+import Community from "../models/community.model";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
+
 import { connectToDB } from "../mongoose";
-import { revalidatePath } from 'next/cache'
+
+export async function fetchUser(userId: string) {
+    try {
+        connectToDB();
+
+        return await User.findOne({ id: userId }).populate({
+            path: "communities",
+            model: Community,
+        });
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`);
+    }
+}
 
 interface Params {
-    userId: string,
-    username: string,
-    name: string,
-    bio: string,
-    image: string,
-    path: string,
+    userId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+    path: string;
 }
 
 export async function updateUser({
     userId,
-    username,
-    name,
     bio,
-    image,
+    name,
     path,
+    username,
+    image,
 }: Params): Promise<void> {
-    connectToDB();
-
     try {
+        connectToDB();
+
         await User.findOneAndUpdate(
             { id: userId },
             {
@@ -35,66 +51,56 @@ export async function updateUser({
                 image,
                 onboarded: true,
             },
-            { upsert: true } // Update if data exists, insert if not
+            { upsert: true }
         );
 
-        if (path === 'profile/edit') {
+        if (path === "/profile/edit") {
             revalidatePath(path);
-        };
+        }
     } catch (error: any) {
-        throw new Error(`Failed to  create/update user: ${error.message}`);
+        throw new Error(`Failed to create/update user: ${error.message}`);
     }
 }
 
-export async function fetchUser(userId: string) {
-    try {
-        connectToDB();
-
-        return await User
-            .findOne({ id: userId })
-        // .populate({
-        //     path: 'communities',
-        //     model: Community
-        // })
-    } catch (error: any) {
-        new Error(`Failed to fetch user: ${error.message}`);
-    }
-}
-
-export async function fetchUserThreads(userId: string) {
+export async function fetchUserPosts(userId: string) {
     try {
         connectToDB();
 
         // Find all threads authored by the user with the given userId
-        // TODO: Populate community
-        const threads = await User.findOne({ id: userId })
-            .populate({
-                path: "threads",
-                model: Thread,
-                populate: [
-                    {
-                        path: "children",
-                        model: Thread,
-                        populate: {
-                            path: "author",
-                            model: User,
-                            select: "name image id", // Select the "name" and "_id" fields from the "User" model
-                        },
+        const threads = await User.findOne({ id: userId }).populate({
+            path: "threads",
+            model: Thread,
+            populate: [
+                {
+                    path: "community",
+                    model: Community,
+                    select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+                },
+                {
+                    path: "children",
+                    model: Thread,
+                    populate: {
+                        path: "author",
+                        model: User,
+                        select: "name image id", // Select the "name" and "_id" fields from the "User" model
                     },
-                ],
-            });
+                },
+            ],
+        });
         return threads;
-    } catch (error: any) {
-        throw new Error(`Failed to fetch user threads: ${error.message}`);
+    } catch (error) {
+        console.error("Error fetching user threads:", error);
+        throw error;
     }
 }
 
-export async function fetchUsers({ 
+// Almost similar to Thead (search + pagination) and Community (search + pagination)
+export async function fetchUsers({
     userId,
     searchString = "",
     pageNumber = 1,
     pageSize = 20,
-    sortBy = "desc"
+    sortBy = "desc",
 }: {
     userId: string;
     searchString?: string;
@@ -105,42 +111,45 @@ export async function fetchUsers({
     try {
         connectToDB();
 
+        // Calculate the number of users to skip based on the page number and page size.
         const skipAmount = (pageNumber - 1) * pageSize;
 
-        // Case insensitive
+        // Create a case-insensitive regular expression for the provided search string.
         const regex = new RegExp(searchString, "i");
 
+        // Create an initial query object to filter users.
         const query: FilterQuery<typeof User> = {
-            // Not equal to userId (Filter out current user)
-            id: { $ne: userId }
-        }
+            id: { $ne: userId }, // Exclude the current user from the results.
+        };
 
-        // Add query if searchString is non empty
-        if (searchString.trim() !== '') {
+        // If the search string is not empty, add the $or operator to match either username or name fields.
+        if (searchString.trim() !== "") {
             query.$or = [
                 { username: { $regex: regex } },
-                { name: { $regex: regex } }
-            ]
+                { name: { $regex: regex } },
+            ];
         }
 
-        // Sorting
+        // Define the sort options for the fetched users based on createdAt field and provided sort order.
         const sortOptions = { createdAt: sortBy };
 
-        // Find user based on queries
         const usersQuery = User.find(query)
             .sort(sortOptions)
             .skip(skipAmount)
             .limit(pageSize);
 
+        // Count the total number of users that match the search criteria (without pagination).
         const totalUsersCount = await User.countDocuments(query);
 
         const users = await usersQuery.exec();
 
+        // Check if there are more users beyond the current page.
         const isNext = totalUsersCount > skipAmount + users.length;
 
         return { users, isNext };
-    } catch (error: any) {
-        throw new Error(`Failed to fetch users: ${error.message}`);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        throw error;
     }
 }
 
@@ -151,23 +160,24 @@ export async function getActivity(userId: string) {
         // Find all threads created by the user
         const userThreads = await Thread.find({ author: userId });
 
-        // Collect all the child thread ids (replies) from the 'children', default acc being []
+        // Collect all the child thread ids (replies) from the 'children' field of each user thread
         const childThreadIds = userThreads.reduce((acc, userThread) => {
-            // Looping through userThread array and adding all the comments to our acc array
             return acc.concat(userThread.children);
         }, []);
 
+        // Find and return the child threads (replies) excluding the ones created by the same user
         const replies = await Thread.find({
             _id: { $in: childThreadIds },
-            author: { $ne: userId }
+            author: { $ne: userId }, // Exclude threads authored by the same user
         }).populate({
-            path: 'author',
+            path: "author",
             model: User,
-            select: 'name image _id'
+            select: "name image _id",
         });
 
         return replies;
-    } catch (error: any) {
-        throw new Error(`Failed to fetch user activity: ${error.message}`)
+    } catch (error) {
+        console.error("Error fetching replies: ", error);
+        throw error;
     }
 }
