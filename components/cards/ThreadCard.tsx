1e863a560a862283c57562137084d0426d0e0d2b
Isaac Song <isaacsongs10@gmail.com>
2024-05-28 14:31:32 -0700

    fix styling for community information

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -111,7 +111,7 @@ const ThreadCard = ({
                 <Link href={`/communities/${community.id}`} className="mt-5 flex items-center">
                     <p className="text-subtle-medium text-gray-1">
                         {formatDateString(createdAt)}
-                        - {community.name} Community
+                        {" "} - {community.name} Community
                     </p>
                     
                     <Image 
