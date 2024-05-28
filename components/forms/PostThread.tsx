76225eb98d4a214b1884c3596a481dabf45931e7
Isaac Song <isaacsongs10@gmail.com>
2024-05-28 13:56:45 -0700

    add organization id if it exists

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/form"
 import { Textarea } from "@/components/ui/textarea"
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useOrganization } from '@clerk/nextjs';
 import { usePathname, useRouter } from 'next/navigation';
 // import { updateUser } from '@/lib/actions/user.actions'; 
 import { ThreadValidation } from '@/lib/validations/thread';
@@ -35,6 +36,7 @@ interface Props {
 function PostThread({ userId }: { userId: string }) {
     const router = useRouter();
     const pathname = usePathname();
+    const { organization } = useOrganization();
     
     const form = useForm({
         resolver: zodResolver(ThreadValidation),
@@ -48,7 +50,7 @@ function PostThread({ userId }: { userId: string }) {
         await createThread({ 
             text: values.thread,
             author: userId,
-            communityId: null,
+            communityId: organization ? organization.id : null,
             path: pathname
         });
 
