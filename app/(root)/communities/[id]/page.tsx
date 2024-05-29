d45de1eec759dba48ce1e339eacf7a6b60c1943f
Isaac Song <isaacsongs10@gmail.com>
2024-05-29 14:48:57 -0700

    update community page with correct info

--- app/(root)/communities/[id]/page.tsx
+++ app/(root)/communities/[id]/page.tsx
@@ -5,6 +5,7 @@ import ProfileHeader from '@/components/shared/ProfileHeader';
 import ThreadsTab from '@/components/shared/ThreadsTab';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { fetchCommunityDetails } from "@/lib/actions/community.actions";
+import UserCard from "@/components/cards/UserCard";
 
 async function Page({ params }: { params: {id: string}}) {
     const user = await currentUser();
@@ -27,7 +28,7 @@ async function Page({ params }: { params: {id: string}}) {
             <div className='mt-9'>
                 <Tabs defaultValue='threads' className='w-full'>
                     <TabsList className='tab'>
-                        {profileTabs.map((tab) => (
+                        {communityTabs.map((tab) => (
                             <TabsTrigger key={tab.label} value={tab.value} className='tab'>
                                 <Image
                                     src={tab.icon}
@@ -40,25 +41,49 @@ async function Page({ params }: { params: {id: string}}) {
 
                                 {tab.label === "Threads" && (
                                     <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
-                                        {userInfo.threads.length}
+                                        {communityDetails.threads.length}
                                     </p>
                                 )}
                             </TabsTrigger>
                         ))}
                     </TabsList>
-                    {profileTabs.map((tab) => (
                         <TabsContent
-                            key={`content-${tab.label}`}
-                            value={tab.value}
+                            value={"threads"}
                             className='w-full text-light-1'
                         >
                             <ThreadsTab
                                 currentUserId={user.id}
-                                accountId={userInfo.id}
-                                accountType='User'
+                                accountId={communityDetails.id}
+                                accountType='Community'
                             />
                         </TabsContent>
-                    ))}
+                        <TabsContent
+                            value={"members"}
+                            className='w-full text-light-1'
+                        >
+                            <section className="mt-9 flex flex-col gap-10">
+                            {communityDetails?.members.map((member: any) => (
+                                <UserCard 
+                                    key={member.id}
+                                    id={member.id}
+                                    name={member.name}
+                                    username={member.username}
+                                    imgUrl={member.image}
+                                    personType="User"
+                                />
+                            ))}
+                            </section>
+                        </TabsContent>
+                        <TabsContent
+                            value={"requests"}
+                            className='w-full text-light-1'
+                        >
+                            <ThreadsTab
+                                currentUserId={user.id}
+                                accountId={communityDetails.id}
+                                accountType='Community'
+                            />
+                        </TabsContent>
                 </Tabs>
             </div>
         </section>
