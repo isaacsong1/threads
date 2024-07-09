9fb69bfa05e120d30a6fa77b77c1be4a4f6f5535
Isaac Song <isaacsongs10@gmail.com>
2024-07-09 00:37:53 -0700

    redirect to onboarding if no user in create thread

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -5,11 +5,9 @@ import { redirect } from 'next/navigation';
 
 async function Page() {
     const user = await currentUser();
-
-    if (!user) return null
+    if (!user) return redirect('/onboarding');
 
     const userInfo = await fetchUser(user.id);
-
     if (!userInfo?.onboarded) redirect('/onboarding');
 
     return (
